68033c2e3bdc735fc8bfd6db815161503cff7e46
kalyangitnew <venkatakalyan9704@gmail.com>
2023-11-18 18:05:41 +0530

    da

--- scripts/add_user_to_group.py
+++ scripts/add_user_to_group.py
@@ -2,31 +2,26 @@ import os
 import sys
 import requests
 
-def add_user_to_group(user_id, group_name):
-    # Your Okta domain and API token
+def add_user_to_group(user_id, group_id):
     okta_domain = os.getenv('OKTA_DOMAIN')
     api_token = os.getenv('OKTA_API_TOKEN')
 
-    # The Okta API endpoint for adding a user to a group
-    url = f"https://{okta_domain}/api/v1/groups/{group_name}/users/{user_id}"
+    url = f"https://{okta_domain}/api/v1/groups/{group_id}/users/{user_id}"
 
-    # The headers for the API request
     headers = {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
         'Authorization': f'SSWS {api_token}',
     }
 
-    # Send the API request
     response = requests.put(url, headers=headers)
 
-    # Check the response
     if response.status_code == 200:
-        print(f"Successfully added user {user_id} to group {group_name}")
+        print(f"Successfully added user {user_id} to group {group_id}.")
     else:
-        print(f"Failed to add user {user_id} to group {group_name}. Response: {response.text}")
+        print(f"Failed to add user {user_id} to group {group_id}. Response: {response.text}")
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
-    group_name = sys.argv[2]
-    add_user_to_group(user_id, group_name)
+    group_id = sys.argv[2]
+    add_user_to_group(user_id, group_id)
